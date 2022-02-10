5d3640159294dda57abcdf7c5cf98d8fa6686fff
Teddy Crepineau <teddy.crepineau@gmail.com>
2022-02-10 22:00:22 +0100

    Implemented Monte Carlo simulation for birthday paradox

--- birthdayParadox/src/BirthdayParadox.java
+++ birthdayParadox/src/BirthdayParadox.java
@@ -10,18 +10,26 @@
  */
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.GregorianCalendar;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.text.SimpleDateFormat;
+import java.util.logging.Logger;
 
 public class BirthdayParadox {
+    Integer simulationSize;
+    Integer groupSize;
+    public static final Logger LOGGER = Logger.getLogger(BirthdayParadox.class.getName());
 
-
-    public BirthdayParadox() {
-        // class constructor
+    public BirthdayParadox(Integer simulationSizeInput, Integer groupSizeInput) {
+        /** Method constructor
+         * @params  simulationSizeInput     number of simulation to run
+         * @params  groupSizeInput          size of the group
+         */
+        this.simulationSize = simulationSizeInput;
+        this.groupSize = groupSizeInput;
     }
 
     public static List<String> getRandomBirthday(int groupSize, int minYear, int maxYear) {
@@ -35,7 +43,7 @@ public class BirthdayParadox {
          */
         
         ArrayList<String> birthdays = new ArrayList<>();
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM-dd");
 
         for (int i = 0; i < groupSize; i++) {
             LocalDate baseDate = LocalDate.now();
@@ -51,17 +59,58 @@ public class BirthdayParadox {
                 baseRandBirthday.getDayOfMonth()
             );
 
-            
+
             String formattedDate = dateFormat.format(randDate);
             birthdays.add(formattedDate);
         }
 
+        Collections.sort(birthdays);
+
         return birthdays;
+    }
+
+    public static Boolean checkSameBirthdate(List<String>  birthdates) {
+        /** Given a list of birth date check the number of people sharing the
+         * same birth date
+         * @param   birthdates  A list of birthdate
+         * @return              number of similar birthdate
+         */
+
+        for (int i = 0; i < birthdates.size() - 1; i++) {
+            String bDateToCompare = birthdates.get(i);
+            if (bDateToCompare.equals(birthdates.get(i+1))) {
+                return true;
+            } 
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
         // main method
-        List<String> bDay = getRandomBirthday(40, 1960, 2022);
-        System.out.println(bDay);
+        
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter the number of simulation to run:  ");
+        Integer simulationSizeInput = reader.nextInt();
+
+        System.out.print("Enter the number of birthday to generate:  ");
+        Integer groupSizeInput = reader.nextInt();
+
+        BirthdayParadox bParadox = new BirthdayParadox(simulationSizeInput, groupSizeInput);
+        Integer sameBDate = 0;
+
+        for (int i = 0; i < bParadox.simulationSize; i++) {
+            if (i % 10000 == 0) {
+                LOGGER.info(String.format("processed %s simultions", i));
+            }
+            List<String> bDay = getRandomBirthday(bParadox.groupSize, 1960, 2022);
+            if (Boolean.TRUE.equals(checkSameBirthdate(bDay))) {
+                sameBDate++;
+            }
+        }
+
+        System.out.println(String.format("Out of %s, %s had a matching birthdate", bParadox.simulationSize, sameBDate));
+        System.out.println(String.format("This gives a probability of %s", (float)sameBDate/bParadox.simulationSize));
+
     }
 }
